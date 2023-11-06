52670fafc859de1ba6494864f4e9fca21a2f8f23
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-11-05 23:57:33 -0300

    ✨cadastro in Formulario and App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([])
+  
+
+  const aoNovoColaboradorAdcionado = (colaborador) => {
+        console.log(colaborador)
+        setColaboradores([...colaboradores, colaborador])
+  }
+  
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoNovoColaboradorCadastrado ={colaborador => aoNovoColaboradorAdcionado(colaborador)}/>
     </div>
   );
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,71 @@
+import { useState } from 'react';
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
-const Formulario = () =>{
+const Formulario = (props) =>{
+    const times=[
+        'Programação',
+        'Front-End',
+        "Data Science",
+        "Devops",
+        "UX e Desing",
+        "Mobile",
+        "Inovacão"
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+   
+
+    const aoSalvar = (event) =>{
+        event.preventDefault()
+        props.aoNovoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+    }
+
     return(
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome do DBServante"/>
-                <CampoTexto label="Cargo" placeholder="Digite o cargo do DBServante" />
-                <CampoTexto label="Imagem" placeholder="Digite o nomne do DBServante"/>
+                <CampoTexto 
+                    obrigatorio
+                    label="Nome" 
+                    placeholder="Digite o nome do DBServante"
+                    valor = {nome}
+                    aoAlterado = {valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio label="Cargo" 
+                    placeholder="Digite o cargo do DBServante"
+                    valor = {cargo}
+                    aoAlterado = {valor => setCargo(valor)}
+                 />
+                <CampoTexto 
+                    obrigatorio
+                    label="Imagem" 
+                    placeholder="Digite o nomne do DBServante"
+                    valor = {imagem}
+                    aoAlterado = {valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                    obrigatorio
+                    label = "Time" 
+                    itens = {times}
+                    valor = {time}
+                    aoAlterado={valor=> setTime(valor)}
+                ></ListaSuspensa>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
