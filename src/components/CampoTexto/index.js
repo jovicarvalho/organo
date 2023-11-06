5b298a170f218fa79b18687d499b01f7e9565a74
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-11-05 23:56:46 -0300

    ✨new props in Campo texto component

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    const aoDigitar = (evento) =>{
+        props.aoAlterado(evento.target.value)
+    }
+
     return(
         <div className="campo-texto" >
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input 
+             required={props.obrigatorio}
+             placeholder={props.placeholder}
+             onChange={aoDigitar}
+             value={props.valor}
+             />
         </div>
     )
 }
