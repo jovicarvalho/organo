79f74e19e8ab7872fb235ef11a6b1ae2ad906e49
João Vitor Nunes Carvalho <joao.carvalho@e-unicred.com.br>
2023-12-03 20:20:32 -0300

    ✨feat: campo cor

---
+++ src/components/Campos/CampoCor/index.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./CampoCor.css";
+
+const CampoCor = (props) => {
+  const aoDigitar = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
+
+  return (
+    <div className="campo-cor">
+      <label>{props.label}</label>
+      <input
+        type={props.tipo}
+        required={props.obrigatorio}
+        placeholder={props.placeholder}
+        onChange={aoDigitar}
+        value={props.valor}
+      />
+    </div>
+  );
+};
+
+export default CampoCor;
